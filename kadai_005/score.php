e94fb1444f1d0e3c401ce9e5243d838b92ee2667
miyamuraaoi <163839133+miyamuraaoi@users.noreply.github.com>
2024-06-22 13:58:43 +0900

    Add files via upload

--- kadai_005/score.php
+++ kadai_005/score.php
@@ -9,7 +9,18 @@
 <body>
   <p>
     <?php
-    $sun = 80 + 60 + 55 + 40 + 100 + 25 + 80 + 95 + 30 + 60;
+    $score1 = 80;
+    $score2 = 60;
+    $score3 = 55;
+    $score4 = 40;
+    $score5 = 100;
+    $score6 = 25;
+    $score7 = 80;
+    $score8 = 95;
+    $score9 = 30;
+    $score10 = 60;
+
+    $sun = $score1 + $score2 + $score3 + $score4 + $score5 + $score6 + $score7 + $score8 + $score9 + $score10;
     $avg = $sun / 10;
     
     echo '合計点'. $sun;
