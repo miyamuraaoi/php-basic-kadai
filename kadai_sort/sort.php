06bd9c3e5fbf66797ec66c9820babf3121add013
miyamuraaoi <163839133+miyamuraaoi@users.noreply.github.com>
2024-06-24 15:40:33 +0900

    Add files via upload

--- kadai_sort/sort.php
+++ kadai_sort/sort.php
@@ -7,32 +7,31 @@
 </head>
 
 <body>
-  <P>
-    <?php
-    // ソートする配列を宣言
-    $nums = [15, 4, 18, 23, 10 ];
-    $order = TRUE;
-
-    function sort_2way() {
-      echo '昇順にソートします。<br>';
-
-      $fruits = array(15, 4, 18, 23, 10);
-      sort($fruits);
-      foreach ($fruits as $key => $val) {
-        echo  $val . "<br>";
-      }
-  
-      echo '降順にソートします。<br>';
-      $fruits = array(15, 4, 18, 23, 10);
-      rsort($fruits);
-      foreach ($fruits as $key => $val) {
-          echo $val . "<br>";
-        
-      }
-    }
+  <p>
+  <?php
+  function sort_2way($order) {
+    // ソートする配列を定義
+    $num = array(15, 4, 18, 23, 10);
     
-    sort_2way();
-    ?>
+    // 昇順または降順のソートを実行
+    if ($order === true) {
+      echo '昇順にソートします。<br>';
+      sort($num);
+    } elseif ($order === false) {
+      echo '降順にソートします。<br>';
+      rsort($num);
+    }
+
+    // ソート結果を表示
+    foreach ($num as $val) {
+      echo $val . "<br>";
+    }
+  }
+
+  // 関数を呼び出す (ここでは例として昇順を指定)
+  sort_2way(true);
+  sort_2way(false);
+  ?>
   </p>
 </body>
 
